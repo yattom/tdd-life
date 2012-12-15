fa91bfc8cb1b36de1076bccc54b1aeaae34e46b7
yattom <tsutomu.yasui@gmail.com>
2012-12-15 19:52:51 +0900

    spiking data structure

--- life.py
+++ life.py
@@ -7,3 +7,14 @@ class GameOfLife(object):
     def dump(self):
         return self.pattern
 
+    def prepare_next_generation(self, cells):
+        for cell in cells:
+            if self.will_born(cell.neighbours):
+                cell.born()
+
+    def will_born(self, neighbours):
+        alive_cells = 0
+        for cell in neighbours:
+            if cell.is_alive():
+                alive_cells += 1
+        return alive_cells == 3
