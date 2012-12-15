75b1b30b5e10a3ad61622dd1e7af865db68a8973
yattom <tsutomu.yasui@gmail.com>
2012-12-15 18:16:08 +0900

    test to set initial pattern

--- life_test.py
+++ life_test.py
@@ -3,8 +3,21 @@
 import unittest
 
 class GameOfLifeTest(unittest.TestCase):
-    def test(self):
-        self.fail()
+    def test_set_initial_pattern(self):
+        initial = '''
+oo.
+o..
+...
+'''
+        game = GameOfLife(pattern=initial)
+
+        expected = '''
+oo.
+o..
+...
+'''
+        actual = game.dump()
+        self.assertEqual(actual, expected)
 
 if __name__=='__main__':
     unittest.main()
